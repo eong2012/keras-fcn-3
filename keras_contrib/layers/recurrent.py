efdede260b9998feb972362f91f863649375c01f
SsaRu <bihong05@hanmail.net>
2017-09-09 16:34:00 +0900

    This repo follow (https://github.com/aurora95/Keras-FCN)  that repo code doesn't work. so i fixed

---
+++ keras_contrib/layers/recurrent.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+import numpy as np
+
+from .. import backend as K
+from .. import activations
+from .. import initializers
+from .. import regularizers
+from keras.engine import Layer
+from keras.engine import InputSpec
+
+from keras.layers.recurrent import _time_distributed_dense
